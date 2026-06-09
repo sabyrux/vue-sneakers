34fa7aa2f9a9f4dfb5e33acac4facc1610122195
Sabyrzhan Zhaslanuly <sabyrux@gmail.com>
2026-06-09 19:10:05 +0500

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+</script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center bg-gray-50">
-    <div class="rounded-2xl bg-white p-8 shadow-lg">
-      <h1 class="mb-3 text-3xl font-bold text-gray-900">You did it!</h1>
-
-      <p class="text-gray-600">
-        Visit
-        <a
-          href="https://vuejs.org/"
-          target="_blank"
-          rel="noopener"
-          class="font-medium text-green-600 transition-colors hover:text-green-700"
-        >
-          vuejs.org
-        </a>
-        to read the documentation.
-      </p>
-    </div>
+  <div class="bg-white w-4/5 mx-auto my-14 h-screen rounded-xl shadow-xl">
+    <AppHeader />
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="border-b border-gray-200 p-12">
+    <div class="flex items-center justify-between">
+      <a href="/" class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" width="40" height="40" />
+        <div class="flex flex-col">
+          <span class="font-bold text-xl/5">VUE SNEAKERS</span>
+          <span>Магазин лучших кроссовок</span>
+        </div>
+      </a>
+      <nav>
+        <ul class="flex items-center gap-8">
+          <li>
+            <a
+              href="/"
+              class="flex items-center gap-2 font-semibold text-gray-500 hover:text-black transition-colors"
+            >
+              <img src="/nav-menu/cart.svg" alt="Корзина" width="18" height="18" />
+              <span>1205 руб.</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="flex items-center gap-2 font-semibold text-gray-500 hover:text-black transition-colors"
+            >
+              <img src="/nav-menu/heart.svg" alt="Закладки" width="18" height="18" />
+              <span>Закладки</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="flex items-center gap-2 font-semibold text-gray-500 hover:text-black transition-colors"
+            >
+              <img src="/nav-menu/profile.svg" alt="Профиль" width="18" height="18" />
+              <span>Профиль</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
